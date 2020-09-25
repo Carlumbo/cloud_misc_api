6152cb157c1a98f97b0ffe9b1df2d9b18b1a4ddf
Carlumbo <Carlpalumbo1@gmail.com>
2020-09-24 22:03:59 -0400

    added special session cookie identifer

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,8 @@ module CloudMiscApi
     config.load_defaults 6.0
     
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_namespace_key'
+    config.middleware.use ActionDispatch::Session::CookieStore, key: 'heroku-session-affinity'
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
